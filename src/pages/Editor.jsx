ed4fcde8c5a5efcdbc90f30576c9e7728409e52e
mariamnozadze <mariamnozadze97@yahoo.com>
2023-03-14 18:26:24 +0400

    - Added dummy text for editor

--- src/pages/Editor.jsx
+++ src/pages/Editor.jsx
@@ -16,6 +16,8 @@ const Editor = () => {
     <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
       <Header category="App" title="Editor" />
       <RichTextEditorComponent>
+        {/* dummy text for editor */}
+        <EditorData /> 
         <Inject services={[HtmlEditor, Toolbar, Image, Link, QuickToolbar]} />
       </RichTextEditorComponent>
     </div>
