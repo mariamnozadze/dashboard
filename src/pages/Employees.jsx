20c6067b781b4cff86fce3945d16c77aa249cb8b
mariamnozadze <mariamnozadze97@yahoo.com>
2023-07-07 13:52:14 +0400

    - Added search for employees

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -5,7 +5,7 @@ import {
   ColumnDirective,
   Page, 
   Search,
-  Inject,
+  Inject, Toolbar
 } from "@syncfusion/ej2-react-grids";
 
 import { employeesData, employeesGrid } from "../data/dummy";
@@ -14,19 +14,20 @@ import { Header } from "../components";
 const Employees = () => {
   return (
     <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
-      <Header category="Page" title="Orders" />
-      <GridComponent 
-      id="gridcomp"
+      <Header category="Page" title="Employees" />
+      <GridComponent
       dataSource={employeesData} 
       allowPaging //to add pagination with Inject
       allowSorting //to allow sorting
+      toolbar={['Search']}
+      width="auto"
        >
         <ColumnsDirective>
           {employeesGrid.map((item, index) => (
             <ColumnDirective key={index} {...item} />
           ))}
         </ColumnsDirective>
-        <Inject services={[Page, Search]} />
+        <Inject services={[Page, Search, Toolbar]} />
       </GridComponent>
     </div>
   );
