63303a10554ddb2450400c95043292a6d275cf84
mariamnozadze <mariamnozadze97@yahoo.com>
2023-07-13 14:52:42 +0400

    - Imported eventsfrom the dummy data

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -20,6 +20,10 @@ const Calendar = () => {
   return (
     <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
       <Header category="App" title="Calendar" />
+      <ScheduleComponent height="650px" eventSettings={{dataSource: scheduleData}}
+      selectedDate={new Date(2021, 0, 10)}>
+        <Inject services={[Day, Week, WorkWeek, Month, Agenda, Resize, DragAndDrop]} />
+      </ScheduleComponent>
     </div>
   );
 };
