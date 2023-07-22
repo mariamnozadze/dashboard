e64c1eaf9d5999d4142b821c20f3e94aff75b819
mariamnozadze <mariamnozadze97@yahoo.com>
2023-07-22 14:25:24 +0400

    - Added chart component sketch

--- src/components/Charts/LineChart.jsx
+++ src/components/Charts/LineChart.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from "react";
+import {
+  ChartComponent,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  Inject,
+  LineSeries,
+  DateTime,
+  Legend,
+  Tooltip,
+} from "@syncfusion/ej2-react-charts";
+import {
+  lineCustomSeries,
+  LinePrimaryXAxis,
+  LinePrimaryYAxis,
+} from "../../data/dummy";
+import { useStateContext } from "../../contexts/ContextProvider";
 
 const LineChart = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <ChartComponent>
+      <Inject />
+    </ChartComponent>
+  );
+};
 
-export default LineChart
+export default LineChart;
